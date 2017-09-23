8dcafcad5ea9cdefda1b012899e9b3c66169198e
David Farrell <shokku.ra@gmail.com>
2017-09-23 17:44:26 +0100

    Add pong example files

---
+++ src/pong/app.cpp
@@ -0,0 +1,21 @@
+#include <pong/app.h>
+#include <polar/system/asset.h>
+#include <polar/system/renderer/gl32.h>
+
+namespace pong {
+	app::app(polar::core::polar &engine) {
+		using namespace polar;
+		engine.addstate("root", [] (core::polar *engine, core::state &st) {
+			st.addsystem<system::asset>();
+			st.addsystem_as<system::renderer::base, system::renderer::gl32, std::vector<std::string>>({"2d"});
+		});
+		engine.addstate("game", [] (core::polar *engine, core::state &st) {
+			IDType leftPaddle, rightPaddle, ball;
+			st.dtors.emplace_back(engine->addobject(&leftPaddle));
+			st.dtors.emplace_back(engine->addobject(&rightPaddle));
+			st.dtors.emplace_back(engine->addobject(&ball));
+		});
+
+		engine.run("root");
+	}
+}
