ef60ed4a74b15d81049ab59834e221407a6f15d8
David Farrell <shokku.ra@gmail.com>
2017-09-24 15:32:56 +0100

    Quit pong on escape key

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -1,18 +1,23 @@
 #include <pong/app.h>
 #include <polar/system/asset.h>
 #include <polar/system/event.h>
+#include <polar/system/input.h>
 #include <polar/system/integrator.h>
 #include <polar/system/renderer/gl32.h>
 #include <polar/component/model.h>
+#include <polar/support/input/key.h>
 
 namespace pong {
 	app::app(polar::core::polar &engine) {
 		using namespace polar;
+		using key_t = support::input::key;
+
 		engine.addstate("root", [] (core::polar *engine, core::state &st) {
 			st.transitions.emplace("forward", Transition{ Push("game") });
 
 			st.addsystem<system::asset>();
 			st.addsystem<system::event>();
+			st.addsystem<system::input>();
 			st.addsystem<system::integrator>();
 			st.addsystem_as<system::renderer::base, system::renderer::gl32>(std::vector<std::string>{"2d"});
 
@@ -31,6 +36,9 @@ namespace pong {
 					{ 0, 1, 0 }
 				}
 			});
+
+			auto inputM = engine->getsystem<system::input>().lock();
+			st.dtors.emplace_back(inputM->on(key_t::Escape, [engine] (key_t) { engine->quit(); }));
 		});
 
 		engine.run("root");
