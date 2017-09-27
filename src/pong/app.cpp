9606a8326d69112051a100877d8915d85599fb3a
David Farrell <shokku.ra@gmail.com>
2017-09-27 02:35:26 +0100

    Update to use position/scale

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -4,6 +4,9 @@
 #include <polar/system/input.h>
 #include <polar/system/integrator.h>
 #include <polar/system/renderer/gl32.h>
+#include <polar/component/position.h>
+#include <polar/component/scale.h>
+#include <polar/component/bounds.h>
 #include <polar/component/model.h>
 #include <polar/support/input/key.h>
 
@@ -29,12 +32,29 @@ namespace pong {
 			st.dtors.emplace_back(engine->addobject(&rightPaddle));
 			st.dtors.emplace_back(engine->addobject(&ball));
 
-			engine->addcomponent<component::model>(leftPaddle, GeometryType::TriangleStrip, component::model::PointsType{
-				{ -0.95, -0.2, 0 },
-				{ -0.90, -0.2, 0 },
-				{ -0.95,  0.2, 0 },
-				{ -0.90,  0.2, 0 }
+			auto model = std::make_shared<component::model>(GeometryType::TriangleStrip, component::model::PointsType{
+				{ -1, -1, 0 },
+				{  1, -1, 0 },
+				{ -1,  1, 0 },
+				{  1,  1, 0 }
 			});
+
+			engine->insertcomponent(leftPaddle, model);
+			engine->addcomponent<component::position>(leftPaddle, Point3(-0.925, 0, 0));
+			engine->addcomponent<component::scale>(leftPaddle, Point3(0.025, 0.2, 0));
+
+			engine->insertcomponent(rightPaddle, model);
+			engine->addcomponent<component::position>(rightPaddle, Point3(0.925, 0, 0));
+			engine->addcomponent<component::scale>(rightPaddle, Point3(0.025, 0.2, 0));
+
+			engine->insertcomponent(ball, model);
+			engine->addcomponent<component::position>(ball);
+			engine->addcomponent<component::scale>(ball, Point3(0.02, 0.02, 0));
+
+			engine->getcomponent<component::position>(ball)->pos.derivative() = Point3(-0.5, -0.1, 0);
+
+			//engine->addcomponent<component::bounds>(leftPaddle,
+			//                                        Point3(-0.925, 0, 0),
 
 			auto inputM = engine->getsystem<system::input>().lock();
 			st.dtors.emplace_back(inputM->on(key_t::Escape, [engine] (key_t) { engine->quit(); }));
