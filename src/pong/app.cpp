7196df4c6fbdfc8dd3c85c06a91aca319359aa53
David Farrell <shokku.ra@gmail.com>
2017-10-12 23:59:37 +0100

    Update to new engine core naming

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -1,8 +1,13 @@
 #include <polar/component/bounds.h>
 #include <polar/component/model.h>
+#include <polar/component/phys.h>
 #include <polar/component/position.h>
 #include <polar/component/scale.h>
 #include <polar/support/input/key.h>
+#include <polar/support/phys/collider/static.h>
+#include <polar/support/phys/detector/box.h>
+#include <polar/support/phys/responder/rigid.h>
+#include <polar/support/phys/responder/stat.h>
 #include <polar/system/asset.h>
 #include <polar/system/event.h>
 #include <polar/system/input.h>
@@ -13,67 +18,70 @@
 namespace pong {
 app::app(polar::core::polar &engine) {
 	using namespace polar;
+	using namespace support::phys;
 	using key_t = support::input::key;
 
 	engine.addstate("root", [](core::polar *engine, core::state &st) {
 		st.transitions.emplace("forward", Transition{Push("game")});
 
-		st.add_system<system::asset>();
-		st.add_system<system::event>();
-		st.add_system<system::input>();
-		st.add_system<system::integrator>();
-		st.add_system_as<system::renderer::base, system::renderer::gl32>(
+		st.add<system::asset>();
+		st.add<system::event>();
+		st.add<system::input>();
+		st.add<system::integrator>();
+		st.add_as<system::renderer::base, system::renderer::gl32>(
 		    std::vector<std::string>{"2d"});
 
 		engine->transition = "forward";
 	});
 	engine.addstate("game", [](core::polar *engine, core::state &st) {
 		IDType leftPaddle, rightPaddle, ball;
-		st.dtors.emplace_back(engine->add_object(&leftPaddle));
-		st.dtors.emplace_back(engine->add_object(&rightPaddle));
-		st.dtors.emplace_back(engine->add_object(&ball));
+		st.dtors.emplace_back(engine->add(&leftPaddle));
+		st.dtors.emplace_back(engine->add(&rightPaddle));
+		st.dtors.emplace_back(engine->add(&ball));
 
 		auto model = std::make_shared<component::model>(
 		    GeometryType::TriangleStrip,
 		    component::model::PointsType{
 		        {-1, -1, 0}, {1, -1, 0}, {-1, 1, 0}, {1, 1, 0}});
 
-		engine->insert_component(leftPaddle, model);
-		engine->insert_component(rightPaddle, model);
-		engine->insert_component(ball, model);
+		engine->insert(leftPaddle, model);
+		engine->insert(rightPaddle, model);
+		engine->insert(ball, model);
 
-		engine->add_component<component::position>(leftPaddle,
-		                                           Point3(-0.925, 0, 0));
-		engine->add_component<component::position>(rightPaddle,
-		                                           Point3(0.925, 0, 0));
-		engine->add_component<component::position>(ball);
+		engine->add<component::position>(leftPaddle, Point3(-0.925, 0, 0));
+		engine->add<component::position>(rightPaddle, Point3(0.925, 0, 0));
+		engine->add<component::position>(ball);
 
-		engine->add_component<component::scale>(leftPaddle,
-		                                        Point3(0.025, 0.2, 0));
-		engine->add_component<component::scale>(rightPaddle,
-		                                        Point3(0.025, 0.2, 0));
-		engine->add_component<component::scale>(ball, Point3(0.02, 0.02, 0));
+		engine->add<component::scale>(leftPaddle, Point3(0.025, 0.2, 0));
+		engine->add<component::scale>(rightPaddle, Point3(0.025, 0.2, 0));
+		engine->add<component::scale>(ball, Point3(0.02, 0.02, 0));
 
-		engine->add_component<component::bounds>(leftPaddle, Point3(-1, -1, 0),
-		                                         Point3(2, 2, 0));
-		engine->add_component<component::bounds>(rightPaddle, Point3(-1, -1, 0),
-		                                         Point3(2, 2, 0));
-		engine->add_component<component::bounds>(ball, Point3(-1, -1, 0),
-		                                         Point3(2, 2, 0));
+		engine->add<component::bounds>(leftPaddle, Point3(-1, -1, 0),
+		                               Point3(2, 2, 0));
+		engine->add<component::bounds>(rightPaddle, Point3(-1, -1, 0),
+		                               Point3(2, 2, 0));
+		engine->add<component::bounds>(ball, Point3(-1, -1, 0),
+		                               Point3(2, 2, 0));
 
-		engine->get_component<component::position>(ball)->pos.derivative() =
+		auto phys = std::make_shared<component::phys>(detector::box(),
+		                                              responder::stat());
+
+		engine->insert<component::phys>(leftPaddle, phys);
+		engine->insert<component::phys>(rightPaddle, phys);
+		engine->add<component::phys>(ball, detector::box(), responder::rigid());
+
+		engine->get<component::position>(ball)->pos.derivative() =
 		    Point3(-0.5, -0.1, 0);
 
-		auto inputM = engine->get_system<system::input>().lock();
+		auto inputM = engine->get<system::input>().lock();
 		st.dtors.emplace_back(
 		    inputM->on(key_t::Escape, [engine](key_t) { engine->quit(); }));
 
 		const Decimal speed = 1.5;
 
 		auto onKey = [engine, speed, leftPaddle](key_t k) {
-			auto paddle =
-			    engine->get_component<component::position>(leftPaddle);
-			auto &y = paddle->pos.derivative()->y;
+			auto paddle = engine->get<component::position>(leftPaddle);
+			auto &y     = paddle->pos.derivative()->y;
 			switch(k) {
 			case key_t::Up:
 				y += speed;
@@ -86,9 +94,8 @@ app::app(polar::core::polar &engine) {
 			}
 		};
 		auto afterKey = [engine, speed, leftPaddle](key_t k) {
-			auto paddle =
-			    engine->get_component<component::position>(leftPaddle);
-			auto &y = paddle->pos.derivative()->y;
+			auto paddle = engine->get<component::position>(leftPaddle);
+			auto &y     = paddle->pos.derivative()->y;
 			switch(k) {
 			case key_t::Up:
 				y -= speed;
