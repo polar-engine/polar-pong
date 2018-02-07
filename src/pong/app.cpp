6d7cf0ee3efca214dbd42470aee1f46eade859ac
David Farrell <shokku.ra@gmail.com>
2018-02-07 10:14:18 +0000

    Bind mouse motion y to left paddle

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -6,13 +6,14 @@
 #include <polar/support/phys/detector/box.h>
 #include <polar/support/phys/responder/rigid.h>
 #include <polar/support/phys/responder/stat.h>
-#include <polar/system/asset.h>
 #include <polar/system/action.h>
+#include <polar/system/asset.h>
 #include <polar/system/event.h>
 #include <polar/system/integrator.h>
+#include <polar/system/keyboard.h>
+#include <polar/system/mouse.h>
 #include <polar/system/phys.h>
 #include <polar/system/renderer/gl32.h>
-#include <polar/system/keyboard.h>
 #include <pong/app.h>
 
 namespace pong {
@@ -28,8 +29,9 @@ namespace pong {
 			st.add<system::asset>();
 			st.add<system::action>();
 			st.add<system::event>();
-			st.add<system::keyboard>();
 			st.add<system::integrator>();
+			st.add<system::keyboard>();
+			st.add<system::mouse>();
 			st.add<system::phys>();
 			st.add_as<system::renderer::base, system::renderer::gl32>(
 			    std::vector<std::string>{"2d"});
@@ -55,8 +57,10 @@ namespace pong {
 			engine->add<component::position>(right_paddle, Point3(0.925, 0, 0));
 			engine->add<component::position>(ball);
 
-			engine->add<component::scale>(left_paddle, Point3(0.025, 0.2, 0));
-			engine->add<component::scale>(right_paddle, Point3(0.025, 0.2, 0));
+			auto scale = std::make_shared<component::scale>(Point3(0.025, 0.2, 0));
+
+			engine->insert(left_paddle,  scale);
+			engine->insert(right_paddle, scale);
 			engine->add<component::scale>(ball, Point3(0.02, 0.02, 0));
 
 			auto phys = std::make_shared<component::phys>(detector::box(),
@@ -72,6 +76,7 @@ namespace pong {
 
 			auto action = engine->get<system::action>().lock();
 			auto kb = engine->get<system::keyboard>().lock();
+			auto mouse = engine->get<system::mouse>().lock();
 
 			const Decimal speed = 1.5;
 			auto on_axis_paddle = [engine, speed](IDType paddle, Decimal delta) {
@@ -92,10 +97,11 @@ namespace pong {
 			st.keep(action->bind(lifetime::when,  kb->action(key::Up),     a_right_paddle,  1));
 			st.keep(action->bind(lifetime::when,  kb->action(key::Down),   a_right_paddle, -1));
 			st.keep(action->bind(lifetime::after, kb->action(key::Escape), a_quit_game));
+			st.keep(action->bind(mouse->action_motion_y(), a_left_paddle));
 
-			auto a_hello_world = action->digital();
-			st.keep(action->bind(lifetime::on, a_hello_world, [] { debugmanager()->info("hello world"); }));
-			st.keep(action->bind(lifetime::when, kb->action(key::H), a_hello_world));
+			st.keep(action->bind(lifetime::when, kb->action(key::H), [] {
+				debugmanager()->notice("hello world");
+			}));
 		});
 
 		engine.run("root");
