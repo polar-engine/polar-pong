276dddfd212ed5b7a3edfe1fe97fcd82a2a4f765
David Farrell <shokku.ra@gmail.com>
2018-02-17 13:19:53 +0000

    Upgrade to type-based action system

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -2,6 +2,8 @@
 #include <polar/component/phys.h>
 #include <polar/component/position.h>
 #include <polar/component/scale.h>
+#include <polar/support/action/keyboard.h>
+#include <polar/support/action/mouse.h>
 #include <polar/support/input/key.h>
 #include <polar/support/phys/detector/box.h>
 #include <polar/support/phys/responder/rigid.h>
@@ -10,8 +12,6 @@
 #include <polar/system/asset.h>
 #include <polar/system/event.h>
 #include <polar/system/integrator.h>
-#include <polar/system/keyboard.h>
-#include <polar/system/mouse.h>
 #include <polar/system/phys.h>
 #include <polar/system/renderer/gl32.h>
 #include <pong/app.h>
@@ -30,8 +30,6 @@ namespace pong {
 			st.add<system::action>();
 			st.add<system::event>();
 			st.add<system::integrator>();
-			st.add<system::keyboard>();
-			st.add<system::mouse>();
 			st.add<system::phys>();
 			st.add_as<system::renderer::base, system::renderer::gl32>(
 			    std::vector<std::string>{"2d"});
@@ -74,9 +72,14 @@ namespace pong {
 			engine->get<component::position>(ball)->pos.derivative() =
 			    Point3(-0.5, -0.1, 0);
 
+			namespace kb    = support::action::keyboard;
+			namespace mouse = support::action::mouse;
+
+			struct a_left_paddle  : system::action::analog {};
+			struct a_right_paddle : system::action::analog {};
+			struct a_quit_game    : system::action::digital {};
+
 			auto action = engine->get<system::action>().lock();
-			auto kb = engine->get<system::keyboard>().lock();
-			auto mouse = engine->get<system::mouse>().lock();
 
 			const Decimal speed = 1.5;
 			auto on_axis_paddle = [engine, speed](IDType paddle, Decimal delta) {
@@ -84,22 +87,18 @@ namespace pong {
 				position->pos.derivative()->y = delta * speed;
 			};
 
-			auto a_left_paddle  = action->analog();
-			auto a_right_paddle = action->analog();
-			auto a_quit_game    = action->digital();
+			st.keep(action->bind<a_left_paddle >(std::bind(on_axis_paddle, left_paddle,  std::placeholders::_1)));
+			st.keep(action->bind<a_right_paddle>(std::bind(on_axis_paddle, right_paddle, std::placeholders::_1)));
+			st.keep(action->bind<a_quit_game>(lifetime::on, [engine] { engine->quit(); }));
 
-			st.keep(action->bind(a_left_paddle,  std::bind(on_axis_paddle, left_paddle,  std::placeholders::_1)));
-			st.keep(action->bind(a_right_paddle, std::bind(on_axis_paddle, right_paddle, std::placeholders::_1)));
-			st.keep(action->bind(lifetime::on, a_quit_game, [engine] { engine->quit(); }));
+			st.keep(action->bind<kb::key<key::W>,      a_left_paddle >(lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::S>,      a_left_paddle >(lifetime::when, -1));
+			st.keep(action->bind<kb::key<key::Up>,     a_right_paddle>(lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::Down>,   a_right_paddle>(lifetime::when, -1));
+			st.keep(action->bind<kb::key<key::Escape>, a_quit_game   >(lifetime::after));
+			st.keep(action->bind<mouse::motion_y, a_left_paddle>());
 
-			st.keep(action->bind(lifetime::when,  kb->action(key::W),      a_left_paddle,   1));
-			st.keep(action->bind(lifetime::when,  kb->action(key::S),      a_left_paddle,  -1));
-			st.keep(action->bind(lifetime::when,  kb->action(key::Up),     a_right_paddle,  1));
-			st.keep(action->bind(lifetime::when,  kb->action(key::Down),   a_right_paddle, -1));
-			st.keep(action->bind(lifetime::after, kb->action(key::Escape), a_quit_game));
-			st.keep(action->bind(mouse->action_motion_y(), a_left_paddle));
-
-			st.keep(action->bind(lifetime::when, kb->action(key::H), [] {
+			st.keep(action->bind<kb::key<key::H>>(lifetime::when, [] {
 				debugmanager()->notice("hello world");
 			}));
 		});
