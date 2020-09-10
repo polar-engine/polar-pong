a86a240aeccf11161d00b61ad88249909fd0cf47
David Farrell <shokku.ra@gmail.com>
2020-09-10 21:05:51 +0100

    Render multiple cabinets displaying arcade camera

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -53,77 +53,124 @@ namespace pong {
 			st.add<system::sched>();
 
 			auto assetM = engine->get<system::asset>().lock();
+			auto cabinet_asset = assetM->get<asset::model>("cabinet");
+			auto monitor_asset = assetM->get<asset::model>("monitor");
 			auto box_asset     = assetM->get<asset::model>("box");
 			auto diffuse_asset = assetM->get<asset::image>("paddle");
 
 			auto win = engine->add();
 			engine->add<component::window>(win, math::point2i(1280, 1280));
 
-			// chromatic aberration
+			// stages
 
-			auto fb_chroma = engine->add();
-			engine->add<component::framebuffer>(fb_chroma, win);
-			engine->add<component::color      >(fb_chroma, math::point4(0.8, 0.5, 0.3, 0));
-
+			auto stage_2d     = engine->add();
+			auto stage_3d     = engine->add();
 			auto stage_chroma = engine->add();
+
+			engine->add<component::stage>(stage_2d,     "2d");
+			engine->add<component::stage>(stage_3d,     "3d");
 			engine->add<component::stage>(stage_chroma, "chroma");
 
-			auto scene_chroma = engine->add();
-			engine->add<component::scene>(scene_chroma);
+			// textures
 
-			auto mat_chroma = engine->add();
-			engine->add<component::material>(mat_chroma, stage_chroma, fb_chroma);
+			auto fb_cctv = engine->add();
+			auto fb_pong = engine->add();
 
-			auto viewport_chroma = engine->add();
-			engine->add<component::model>(viewport_chroma, box_asset, scene_chroma, mat_chroma);
-			//engine->add<component::orientation>(viewport_chroma)->orient.derivative(0) = math::point3(0, math::PI_BY<2>, 0);
+			engine->add<component::framebuffer>(fb_cctv, win, true, true);
 
-			auto camera_chroma = engine->add();
-			auto proj = math::perspective(math::point2(1280, 1280), 0.1, 1000);
-			engine->add<component::camera     >(camera_chroma, scene_chroma, win, proj);
-			engine->add<component::position   >(camera_chroma)->pos.derivative(0) = math::point3(0, 0, 0.5f);
-
-			// main
-
-			auto stage_main = engine->add();
-			engine->add<component::stage>(stage_main, "2d");
-
-			auto scene_main = engine->add();
-			engine->add<component::scene>(scene_main);
+			engine->add<component::framebuffer>(fb_pong, win);
+			engine->add<component::color      >(fb_pong, math::point4(0.8, 0.5, 0.3, 0));
 
 			auto tex_box = engine->add();
+
 			engine->add<component::texture>(tex_box, diffuse_asset);
 
-			auto mat_box = engine->add();
-			engine->add<component::material>(mat_box, stage_main, tex_box);
+			// materials
+
+			auto mat_cabinet = engine->add();
+			auto mat_box     = engine->add();
+			auto mat_chroma  = engine->add();
+
+			engine->add<component::material>(mat_cabinet, stage_3d,     tex_box);
+			engine->add<component::material>(mat_box,     stage_2d,     tex_box);
+			engine->add<component::material>(mat_chroma,  stage_chroma, fb_cctv);
+
+			// scenes
+
+			auto scene_arcade = engine->add();
+			auto scene_pong = engine->add();
+
+			engine->add<component::scene>(scene_arcade);
+			engine->add<component::scene>(scene_pong);
+
+			// cameras
+
+			auto camera_arcade = engine->add();
+			auto camera_cctv   = engine->add();
+			auto camera_pong   = engine->add();
+
+			auto proj_arcade = math::perspective(math::point2(1280, 1280), 0.1, 1000);
+			engine->add<component::camera  >(camera_arcade, scene_arcade, win, proj_arcade);
+			engine->add<component::position>(camera_arcade, math::point3(0, 1.2f, 4));
+
+			auto proj_cctv = math::perspective(math::point2(1280, 1280), 0.1, 1000);
+			engine->add<component::camera     >(camera_cctv, scene_arcade, fb_cctv, proj_cctv);
+			engine->add<component::position   >(camera_cctv, math::point3(1, 1.6f, 1.5f));
+			engine->add<component::orientation>(camera_cctv, math::point3(0, glm::radians(-20.0f), 0));
+
+			//engine->add<component::camera>(camera_pong, scene_pong, fb_pong);
+
+			st.keep(camera_arcade);
+			st.keep(camera_cctv);
+			st.keep(camera_pong);
+
+			// arcade
+
+			for(size_t row = 0; row < 10; ++row) {
+				math::decimal z = row * -4.0f;
+				for(size_t col = 0; col < 10; ++col) {
+					math::decimal x = col * 2.0f - 10.0f;
+
+					auto cabinet = engine->add();
+					engine->add<component::position>(cabinet, math::point3(x, 0, z));
+					engine->add<component::model   >(cabinet, cabinet_asset, scene_arcade, mat_cabinet);
+
+					auto viewport_pong = engine->add();
+					engine->add<component::position>(viewport_pong, math::point3(x, 0, z));
+					engine->add<component::model   >(viewport_pong, monitor_asset, scene_arcade, mat_chroma);
+
+					if(row > 0) {
+						engine->add<component::orientation>(cabinet)->orient.derivative(0) = math::point3(0, math::PI_OVER<2>, 0);
+						engine->add<component::orientation>(viewport_pong)->orient.derivative(0) = math::point3(0, math::PI_OVER<2>, 0);
+					}
+
+					st.keep(cabinet);
+					st.keep(viewport_pong);
+				}
+			}
+
+			// pong
 
 			auto ball = engine->add();
 			engine->add<component::position>(ball)->pos.derivative(0) = math::point3(-0.5, -0.1, 0);
-			engine->add<component::scale   >(ball, math::point3(0.2, 0.2, 0));
+			engine->add<component::scale   >(ball, math::point3(0.02, 0.02, 0));
 			engine->add<component::phys    >(ball, detector::box(), responder::rigid());
-			engine->add<component::model   >(ball, box_asset, scene_main, mat_box);
+			engine->add<component::model   >(ball, box_asset, scene_pong, mat_box);
+			st.keep(ball);
 
 			auto left_paddle = engine->add();
 			engine->add<component::position>(left_paddle, math::point3(-0.925, 0, 0));
 			engine->add<component::scale   >(left_paddle, math::point3(0.025, 0.2, 0));
 			engine->add<component::phys    >(left_paddle, detector::box(), responder::stat());
-			engine->add<component::model   >(left_paddle, box_asset, scene_main, mat_box);
+			engine->add<component::model   >(left_paddle, box_asset, scene_pong, mat_box);
+			st.keep(left_paddle);
 
 			auto right_paddle = engine->add();
 			engine->add<component::position>(right_paddle, math::point3( 0.925, 0, 0));
 			engine->add<component::scale   >(right_paddle, math::point3(0.025, 0.2, 0));
 			engine->add<component::phys    >(right_paddle, detector::box(), responder::stat());
-			engine->add<component::model   >(right_paddle, box_asset, scene_main, mat_box);
-
-			auto camera_main = engine->add();
-			engine->add<component::camera>(camera_main, scene_main, fb_chroma);
-
-			st.keep(viewport_chroma);
-			st.keep(ball);
-			st.keep(left_paddle);
+			engine->add<component::model   >(right_paddle, box_asset, scene_pong, mat_box);
 			st.keep(right_paddle);
-			st.keep(camera_chroma);
-			st.keep(camera_main);
 
 			// actions
 
