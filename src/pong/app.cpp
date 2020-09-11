36de9bd21501d78435d9cd2cf3e0b116671241e5
David Farrell <shokku.ra@gmail.com>
2020-09-11 02:13:24 +0100

    Add curved monitor and moveable CCTV

--- src/pong/app.cpp
+++ src/pong/app.cpp
@@ -55,6 +55,7 @@ namespace pong {
 			auto assetM = engine->get<system::asset>().lock();
 			auto cabinet_asset = assetM->get<asset::model>("cabinet");
 			auto monitor_asset = assetM->get<asset::model>("monitor");
+			auto portal_asset  = assetM->get<asset::model>("portal");
 			auto box_asset     = assetM->get<asset::model>("box");
 			auto diffuse_asset = assetM->get<asset::image>("paddle");
 
@@ -63,13 +64,15 @@ namespace pong {
 
 			// stages
 
-			auto stage_2d     = engine->add();
-			auto stage_3d     = engine->add();
-			auto stage_chroma = engine->add();
+			auto stage_2d      = engine->add();
+			auto stage_3d      = engine->add();
+			auto stage_monitor = engine->add();
+			auto stage_chroma  = engine->add();
 
-			engine->add<component::stage>(stage_2d,     "2d");
-			engine->add<component::stage>(stage_3d,     "3d");
-			engine->add<component::stage>(stage_chroma, "chroma");
+			engine->add<component::stage>(stage_2d,      "2d");
+			engine->add<component::stage>(stage_3d,      "3d");
+			engine->add<component::stage>(stage_monitor, "monitor");
+			engine->add<component::stage>(stage_chroma,  "chroma");
 
 			// textures
 
@@ -88,12 +91,16 @@ namespace pong {
 			// materials
 
 			auto mat_cabinet = engine->add();
+			auto mat_monitor = engine->add();
+			auto mat_portal  = engine->add();
 			auto mat_box     = engine->add();
 			auto mat_chroma  = engine->add();
 
-			engine->add<component::material>(mat_cabinet, stage_3d,     tex_box);
-			engine->add<component::material>(mat_box,     stage_2d,     tex_box);
-			engine->add<component::material>(mat_chroma,  stage_chroma, fb_cctv);
+			engine->add<component::material>(mat_cabinet, stage_3d,      tex_box);
+			engine->add<component::material>(mat_monitor, stage_monitor, fb_cctv);
+			engine->add<component::material>(mat_portal,  stage_monitor, fb_cctv);
+			engine->add<component::material>(mat_box,     stage_2d,      tex_box);
+			engine->add<component::material>(mat_chroma,  stage_chroma,  fb_cctv);
 
 			// scenes
 
@@ -111,12 +118,14 @@ namespace pong {
 
 			auto proj_arcade = math::perspective(math::point2(1280, 1280), 0.1, 1000);
 			engine->add<component::camera  >(camera_arcade, scene_arcade, win, proj_arcade);
-			engine->add<component::position>(camera_arcade, math::point3(0, 1.2f, 4));
+			engine->add<component::position>(camera_arcade, math::point3(0, 1.2f, 2));
+			engine->add<component::model   >(camera_arcade, cabinet_asset, scene_arcade, mat_cabinet);
 
 			auto proj_cctv = math::perspective(math::point2(1280, 1280), 0.1, 1000);
 			engine->add<component::camera     >(camera_cctv, scene_arcade, fb_cctv, proj_cctv);
 			engine->add<component::position   >(camera_cctv, math::point3(1, 1.6f, 1.5f));
 			engine->add<component::orientation>(camera_cctv, math::point3(0, glm::radians(-20.0f), 0));
+			engine->add<component::model      >(camera_cctv, portal_asset, scene_arcade, mat_box);
 
 			//engine->add<component::camera>(camera_pong, scene_pong, fb_pong);
 
@@ -126,10 +135,10 @@ namespace pong {
 
 			// arcade
 
-			for(size_t row = 0; row < 10; ++row) {
+			for(size_t row = 0; row < 3; ++row) {
 				math::decimal z = row * -4.0f;
-				for(size_t col = 0; col < 10; ++col) {
-					math::decimal x = col * 2.0f - 10.0f;
+				for(size_t col = 0; col < 3; ++col) {
+					math::decimal x = (math::decimal(col) - 3 / 2) * 2;
 
 					auto cabinet = engine->add();
 					engine->add<component::position>(cabinet, math::point3(x, 0, z));
@@ -137,17 +146,29 @@ namespace pong {
 
 					auto viewport_pong = engine->add();
 					engine->add<component::position>(viewport_pong, math::point3(x, 0, z));
-					engine->add<component::model   >(viewport_pong, monitor_asset, scene_arcade, mat_chroma);
+					engine->add<component::model   >(viewport_pong, monitor_asset, scene_arcade, mat_monitor);
 
+					/*
 					if(row > 0) {
 						engine->add<component::orientation>(cabinet)->orient.derivative(0) = math::point3(0, math::PI_OVER<2>, 0);
 						engine->add<component::orientation>(viewport_pong)->orient.derivative(0) = math::point3(0, math::PI_OVER<2>, 0);
 					}
+					*/
 
 					st.keep(cabinet);
 					st.keep(viewport_pong);
 				}
 			}
+
+			// arcade portals
+
+			/*
+			auto portal1 = engine->add();
+			engine->add<component::position>(portal1, math::point3(0, 0, -10));
+			engine->add<component::scale   >(portal1, math::point3(4, 4, 4));
+			engine->add<component::model   >(portal1, portal_asset, scene_arcade, mat_portal);
+			st.keep(portal1);
+			*/
 
 			// pong
 
@@ -177,11 +198,22 @@ namespace pong {
 			namespace kb    = support::action::keyboard;
 			namespace mouse = support::action::mouse;
 
-			struct a_left_paddle  : system::action::analog {};
-			struct a_right_paddle : system::action::analog {};
+			auto action = engine->get<system::action>().lock();
+
 			struct a_quit_game    : system::action::digital {};
 
-			auto action = engine->get<system::action>().lock();
+			st.keep(action->bind<a_quit_game>(lifetime::on, [engine](auto) { engine->quit(); }));
+
+			st.keep(action->bind<kb::key<key::Escape>, a_quit_game   >(lifetime::after));
+			st.keep(action->bind<kb::key<key::H>>(lifetime::when, [](auto) {
+				polar::log()->notice("pong", "hello world");
+			}));
+
+			// pong actions
+
+			/*
+			struct a_left_paddle  : system::action::analog {};
+			struct a_right_paddle : system::action::analog {};
 
 			const math::decimal speed = 1.5;
 			auto on_axis_paddle = [engine, speed](core::ref paddle, math::decimal delta) {
@@ -191,18 +223,41 @@ namespace pong {
 
 			st.keep(action->bind<a_left_paddle >(std::bind(on_axis_paddle, left_paddle,  std::placeholders::_2)));
 			st.keep(action->bind<a_right_paddle>(std::bind(on_axis_paddle, right_paddle, std::placeholders::_2)));
-			st.keep(action->bind<a_quit_game>(lifetime::on, [engine](auto) { engine->quit(); }));
 
 			st.keep(action->bind<kb::key<key::W>,      a_left_paddle >(lifetime::when,  1));
 			st.keep(action->bind<kb::key<key::S>,      a_left_paddle >(lifetime::when, -1));
 			st.keep(action->bind<kb::key<key::Up>,     a_right_paddle>(lifetime::when,  1));
 			st.keep(action->bind<kb::key<key::Down>,   a_right_paddle>(lifetime::when, -1));
-			st.keep(action->bind<kb::key<key::Escape>, a_quit_game   >(lifetime::after));
 			//st.keep(action->bind<mouse::motion_y, a_left_paddle>());
+			*/
 
-			st.keep(action->bind<kb::key<key::H>>(lifetime::when, [](auto) {
-				polar::log()->notice("pong", "hello world");
-			}));
+			// arcade actions
+
+			struct a_camera_x : system::action::analog {};
+			struct a_camera_z : system::action::analog {};
+
+			auto on_camera_x = [engine](auto camera_ref, auto delta) {
+				auto pos = engine->mutate<component::position>(camera_ref);
+				pos->pos.derivative(0)->x = delta * 4;
+			};
+
+			auto on_camera_z = [engine](auto camera_ref, auto delta) {
+				auto pos = engine->mutate<component::position>(camera_ref);
+				pos->pos.derivative(0)->z = delta * 4;
+			};
+
+			st.keep(action->bind<a_camera_x>(on_camera_x));
+			st.keep(action->bind<a_camera_z>(on_camera_z));
+
+			st.keep(action->bind<kb::key<key::S>, a_camera_z>(camera_arcade, lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::W>, a_camera_z>(camera_arcade, lifetime::when, -1));
+			st.keep(action->bind<kb::key<key::D>, a_camera_x>(camera_arcade, lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::A>, a_camera_x>(camera_arcade, lifetime::when, -1));
+
+			st.keep(action->bind<kb::key<key::K>, a_camera_z>(camera_cctv, lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::I>, a_camera_z>(camera_cctv, lifetime::when, -1));
+			st.keep(action->bind<kb::key<key::L>, a_camera_x>(camera_cctv, lifetime::when,  1));
+			st.keep(action->bind<kb::key<key::J>, a_camera_x>(camera_cctv, lifetime::when, -1));
 		});
 
 		engine.run("root");
